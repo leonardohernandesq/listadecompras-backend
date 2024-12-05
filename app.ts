e2c5283a89091934c83a4b9d91d7344cfdbb3620
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2024-12-05 23:41:00 +0000

    First commit

--- app.ts
+++ app.ts
@@ -11,7 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({
+    credentials: true,
+    origin: "*"
+}));
 
 
 app.use(router);
